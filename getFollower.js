da0e8db38b81fb49c220033bf61e1c6f11d5ada9
Achim Mertens <achim_mertens@gmx.de>
2023-11-14 08:18:31 +0100

    chore: replaces .then with try and catch

--- getFollower.js
+++ getFollower.js
@@ -1,23 +1,16 @@
-// Importieren Sie die dhive-Bibliothek
 const dhive = require('@hiveio/dhive');
 
 async function getFollower(username) {
-// Erstellen Sie einen neuen Client
-let client = new dhive.Client('https://api.hive.blog');
+  let client = new dhive.Client('https://api.hive.blog');
 
-// Definieren Sie den Benutzernamen
-//let username = 'achimmertens';
-
-// Rufen Sie die Follower-Liste ab
-client.call('condenser_api', 'get_follow_count', [username])
-    .then(result => {
-        console.log('Anzahl der Follower: ', result.follower_count);
-        return result.follower_count
-    })
-    .catch(err => {
-        console.error('Es gab einen Fehler beim Abrufen der Follower-Anzahl: ', err);
-        return err;
-    });  
-};
+  try {
+    const result = await client.call('condenser_api', 'get_follow_count', [username]);
+    console.log('Anzahl der Follower: ', result.follower_count);
+    return result.follower_count;
+  } catch (err) {
+    console.error('Es gab einen Fehler beim Abrufen der Follower-Anzahl: ', err);
+    throw err;
+  }
+}
 
 module.exports = getFollower;
